33be11794014f40ac16bd5f2af35f5111f1e6ba3
achauvin10 <chauvinantoine10@gmail.com>
2023-08-17 18:03:08 +0200

    just correcting some stuff from the previous commit, more precisely the few useless print used for debugging

--- pomodoro_app/timer/timer.py
+++ pomodoro_app/timer/timer.py
@@ -9,7 +9,7 @@ class Timer(ft.UserControl):
             self.initialtime=initialtime
             self.runningtimer=False
             #self.timer=ft.Text(str(self.initialtime),style=ft.TextThemeStyle.DISPLAY_LARGE,data= self.initialtime)
-            self.timer=ft.TextField(label="counter", value=str(self.initialtime),on_change=self.set_timer,on_submit=self.timer_control,data=self.initialtime)
+            self.timer=ft.TextField(label="counter", value=str(self.initialtime),on_change=self.set_timer, on_submit=self.timer_control, data=self.initialtime)
             self.startbutton=ft.ElevatedButton(text="Start timer !",on_click=self.timer_control)
 
     def build(self):
@@ -29,14 +29,12 @@ class Timer(ft.UserControl):
                 
     
     def timer_control(self,e):
-        pass
         
         if not self.runningtimer:
             self.runningtimer=True
             self.timer.disabled=True
             self.counter()
         else:
-            print(self.timer.value)
             self.timer.data=float(self.timer.value)
             self.runningtimer=False
             self.timer.disabled=False
@@ -44,7 +42,4 @@ class Timer(ft.UserControl):
             
 
     def set_timer(self,e):
-        print(self.timer.value)
         self.timer.data=float(self.timer.value)
-        print(self.timer.data)
-
